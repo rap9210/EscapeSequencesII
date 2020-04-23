ec8d55e2533946dd1752103dab2670189d6332b5
rap9210 <rap9210@gmail.com>
2020-04-22 23:46:00 -0400

    Escape Sequences II

---
+++ src/EscapeSequencesII.java
@@ -0,0 +1,7 @@
+public class EscapeSequencesII {
+    public static void main(String[] args){
+        System.out.println("Cost\tQuantity\tTotal\n$1,000.00\t4\t$4,000.00");
+        System.out.println("$   50.00\t8\t  $400.00\n__________________________");
+        System.out.println("TOTAL:\t\t12\t$4,400.00");
+    }
+}
